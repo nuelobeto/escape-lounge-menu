b1d3b42f112aa1832f32a8964baece4c69178e8c
nuelobeto <nuelobeto@gmail.com>
2024-02-13 08:15:12 +0100

    fixed fonts

--- src/app-navigation/AppRouter.tsx
+++ src/app-navigation/AppRouter.tsx
@@ -7,39 +7,39 @@ import useMenu from "../store/useMenu";
 import { useEffect } from "react";
 
 const AppRouter = () => {
-  const { getMenu, loading } = useMenu((state) => state);
-  const location = useLocation();
-  const menuId = new URLSearchParams(location.search).get("id");
-  const savedId: string | null = localStorage.getItem("menu_id");
-  const parsedMenuId: string | undefined = savedId
-    ? JSON.parse(savedId)
-    : undefined;
+	const { getMenu, loading } = useMenu((state) => state);
+	const location = useLocation();
+	const menuId = new URLSearchParams(location.search).get("id");
+	const savedId: string | null = localStorage.getItem("menu_id");
+	const parsedMenuId: string | undefined = savedId
+		? JSON.parse(savedId)
+		: undefined;
 
-  useEffect(() => {
-    if (menuId) {
-      localStorage.setItem("menu_id", JSON.stringify(menuId));
-    }
-  }, [menuId]);
+	useEffect(() => {
+		if (menuId) {
+			localStorage.setItem("menu_id", JSON.stringify(menuId));
+		}
+	}, [menuId]);
 
-  useEffect(() => {
-    if (menuId) {
-      getMenu(menuId);
-    }
-    if (parsedMenuId) {
-      getMenu(parsedMenuId);
-    }
-  }, [menuId, parsedMenuId]);
+	useEffect(() => {
+		if (menuId) {
+			getMenu(menuId);
+		}
+		if (parsedMenuId) {
+			getMenu(parsedMenuId);
+		}
+	}, [menuId, parsedMenuId, getMenu]);
 
-  return (
-    <div className="relative max-w-[800px] w-full h-screen m-auto bg-illustration">
-      <Routes>
-        <Route path={ROUTES.home} element={<Home />} />
-        <Route path={`${ROUTES.category}/:slug`} element={<Category />} />
-      </Routes>
+	return (
+		<div className="relative max-w-[800px] w-full h-screen m-auto bg-illustration">
+			<Routes>
+				<Route path={ROUTES.home} element={<Home />} />
+				<Route path={`${ROUTES.category}/:slug`} element={<Category />} />
+			</Routes>
 
-      {loading && <Preloader />}
-    </div>
-  );
+			{loading && <Preloader />}
+		</div>
+	);
 };
 
 export default AppRouter;
--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -1,56 +1,52 @@
 import { RiCloseFill } from "react-icons/ri";
 
 type ModalWrapperProps = {
-  children: React.ReactNode;
-  title?: string;
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	children: React.ReactNode;
+	title?: string;
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const ModalWrapper = ({
-  children,
-  title,
-  isOpen,
-  setIsOpen,
+	children,
+	title,
+	isOpen,
+	setIsOpen,
 }: ModalWrapperProps) => {
-  const handlePropagation = (e: any) => {
-    e.stopPropagation();
-  };
+	const handlePropagation = (e: any) => {
+		e.stopPropagation();
+	};
 
-  return (
-    <div
-      className={`fixed w-full h-screen top-0 left-0 z-10 transition duration-300 ${
-        isOpen ? "bg-[#0000009d]" : "pointer-events-none"
-      }`}
-      onClick={() => setIsOpen(false)}
-    >
-      <div
-        className={`absolute w-full bg-neutral-950 rounded-t-3xl bottom-0 left-0 transition duration-[0.4s] ${
-          isOpen ? " translate-y-0" : "translate-y-[100%]"
-        }`}
-        onClick={(e) => handlePropagation(e)}
-      >
-        <div
-          className={`p-4 flex items-center rounded-t-3xl border-b-2 border-neutral-900 ${
-            title ? "justify-between" : "justify-end"
-          }`}
-        >
-          {title && (
-            <p className="text-[18px] font-semibold text-neutral-400">
-              {title}
-            </p>
-          )}
-          <button
-            className="text-[30px] p-1 rounded-full hover:bg-neutral-900 text-antique-gold transition"
-            onClick={() => setIsOpen(false)}
-          >
-            <RiCloseFill />
-          </button>
-        </div>
-        <div className="max-h-[80vh] overflow-auto hide-scroll">{children}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`fixed w-full max-w-[800px] h-screen top-0 left-[50%] translate-x-[-50%] z-10 transition duration-300 ${
+				isOpen ? "bg-[#0000009d]" : "pointer-events-none"
+			}`}
+			onClick={() => setIsOpen(false)}>
+			<div
+				className={`absolute w-full bg-neutral-950 rounded-t-3xl bottom-0 left-0 transition duration-[0.4s] ${
+					isOpen ? " translate-y-0" : "translate-y-[100%]"
+				}`}
+				onClick={(e) => handlePropagation(e)}>
+				<div
+					className={`p-4 flex items-center rounded-t-3xl border-b-2 border-neutral-900 ${
+						title ? "justify-between" : "justify-end"
+					}`}>
+					{title && (
+						<p className="text-[18px] font-semibold text-neutral-400">
+							{title}
+						</p>
+					)}
+					<button
+						className="text-[30px] p-1 rounded-full hover:bg-neutral-900 text-antique-gold transition"
+						onClick={() => setIsOpen(false)}>
+						<RiCloseFill />
+					</button>
+				</div>
+				<div className="max-h-[80vh] overflow-auto hide-scroll">{children}</div>
+			</div>
+		</div>
+	);
 };
 
 export default ModalWrapper;
