83477526cf083ab08f2bd418ad16535de9384977
obeto emmanuel <nuelobeto@gmail.com>
2024-02-14 05:40:44 +0100

    currency fix on menu modal

--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -2,29 +2,30 @@ import { MenuItemT } from "../types/types";
 import ModalWrapper from "./ModalWrapper";
 
 type MenuModalProps = {
-  openMenuModal: boolean;
-  setOpenMenuModal: React.Dispatch<React.SetStateAction<boolean>>;
-  item: MenuItemT | null;
+	openMenuModal: boolean;
+	setOpenMenuModal: React.Dispatch<React.SetStateAction<boolean>>;
+	item: MenuItemT | null;
 };
 
 const MenuModal = ({
-  openMenuModal,
-  setOpenMenuModal,
-  item,
+	openMenuModal,
+	setOpenMenuModal,
+	item,
 }: MenuModalProps) => {
-  return (
-    <ModalWrapper isOpen={openMenuModal} setIsOpen={setOpenMenuModal}>
-      <div className="p-6 flex flex-col gap-2">
-        <h3 className="font-bold text-neutral-300">
-          {item?.item_name.toUpperCase()}
-        </h3>
-        <p className="text-[14px] text-neutral-400">{item?.item_description}</p>
-        <p className="text-antique-gold text-[15px]">
-          ₦{Number(item?.item_amount).toFixed(2)}
-        </p>
-      </div>
-    </ModalWrapper>
-  );
+	return (
+		<ModalWrapper isOpen={openMenuModal} setIsOpen={setOpenMenuModal}>
+			<div className="p-6 flex flex-col gap-2">
+				<h3 className="font-bold text-neutral-300">
+					{item?.item_name.toUpperCase()}
+				</h3>
+				<p className="text-[14px] text-neutral-400">{item?.item_description}</p>
+				<p className="text-antique-gold text-[15px]">
+					<span className="text-[12px]">₦</span>
+					{item?.item_amount}
+				</p>
+			</div>
+		</ModalWrapper>
+	);
 };
 
 export default MenuModal;
